59219f635cd5987245845929439b35809c6522f6
Hajime Miyazaki <vsj2015.us@gmail.com>
2024-03-04 22:43:32 -0800

    Squashed 'TodoApp/' content from commit 2ec5e38
    git-subtree-dir: TodoApp
    git-subtree-split: 2ec5e38d10bb928eab22a13242d9a1a7f06c9b34

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by SFSU on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
