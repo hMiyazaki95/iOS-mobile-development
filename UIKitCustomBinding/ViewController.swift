d6c9cc3706b140ef6b4c9b59afc459241a0ea098
Hajime Miyazaki <vsj2015.us@gmail.com>
2024-03-04 21:58:28 -0800

    Squashed 'UIKitCustomBinding/' content from commit 53ddf09
    git-subtree-dir: UIKitCustomBinding
    git-subtree-split: 53ddf09332ba4e6ed578f7b18ead8881a9a98af9

---
+++ UIKitCustomBinding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitCustomBinding
+//
+//  Created by Hajime Miyazaki on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
