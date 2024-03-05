0dc5963a7a69efb15039672c5ffa1d767bd4c0ce
Hajime Miyazaki <vsj2015.us@gmail.com>
2024-03-04 22:42:45 -0800

    Squashed 'Tasks-part-2/' content from commit 16b8179
    git-subtree-dir: Tasks-part-2
    git-subtree-split: 16b8179706859d0b12ea83f4d05f568a0f326672

---
+++ Tasks/ViewControlelrs/TaskCell.swift
@@ -0,0 +1,23 @@
+//  Created by SFSU on 10/24/23.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .green
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // Reset all properties that might not be set in
+        // cellForRowAtIndexPath
+        backgroundColor = .green
+    }
+}
