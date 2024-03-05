4abc2d349dac027e07da056cf481494fdc83d80a
Hajime Miyazaki <vsj2015.us@gmail.com>
2024-03-04 22:40:35 -0800

    Squashed 'MuniApp/' content from commit dda67db
    git-subtree-dir: MuniApp
    git-subtree-split: dda67db09ad9d6bf8c24f370e9bd238d8074a1cc

---
+++ MuniApp/MuniAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  MuniAppApp.swift
+//  MuniApp
+//
+
+import SwiftUI
+
+struct Constants {
+//    static var apiKey: String = "asdfljas;ldfj;aljdsf"
+}
+
+@main
+struct MuniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
